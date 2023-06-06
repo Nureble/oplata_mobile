8644f5eae009b879087f7fde153272fed481002d
Nureble <nureble@yandex.ru>
2023-06-06 13:04:02 +0400

    fix: remove console.log and add types

--- src/types/types.ts
+++ src/types/types.ts
@@ -2,3 +2,10 @@ export interface Operator {
   id: string;
   name: string;
 }
+
+export enum Statuses {
+  SUCCESS = "SUCCESS",
+  ERROR = "ERROR",
+  DEFAULT = "DEFAULT",
+  LOADING = "LOADING",
+}
